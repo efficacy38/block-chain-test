3a9b88aca0406fff8155990c85f8db304d0e4692
jerry <efficacy38@gmail.com>
2023-04-03 20:22:46 +0800

    contract added, but recipient can't be specified, only send to the initiator of contract

--- src/views/FaucetView.vue
+++ src/views/FaucetView.vue
@@ -1,97 +1,176 @@
 <template>
-  <div class="container h-100">
-    <div class="row justify-content-center align-items-start text-center pt-5">
-      <h1 class="text-white fw-light lh-1 mb-5">JERRY'S FAUCET</h1>
-      <div class="col-7 card border-0 shadow rounded-xs py-5">
-        <div class="row">
-          <div class="col-9">
-            <input type="text" class="form-control item" placeholder="your wallet address" />
-          </div>
-          <div class="col-3">
-            <button type="button" class="btn btn-primary w-100">Send the Eth</button>
-          </div>
+    <div class="container h-100">
+        <div
+            class="row justify-content-center align-items-start text-center pt-5"
+        >
+            <h1 class="text-white fw-light lh-1 mb-5">JERRY'S FAUCET</h1>
+            <div class="col-7 card border-0 shadow rounded-xs py-3">
+                <div class="row">
+                    <div
+                        class="col m-3 w-100 alert alert-primary"
+                        v-if="message"
+                    >
+                        {{ this.message }}
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-9">
+                        <input
+                            type="text"
+                            class="form-control item"
+                            placeholder="your wallet address"
+                            v-model="recipient"
+                        />
+                    </div>
+                    <div class="col-3">
+                        <button
+                            type="button"
+                            class="btn btn-primary w-100"
+                            @click="withdarwHandler"
+                        >
+                            Send the Eth
+                        </button>
+                    </div>
+                </div>
+                <hr />
+                <div>
+                    <h3>Your transations</h3>
+                    <table class="table text-start">
+                        <thead>
+                            <tr>
+                                <th scope="col" class="col-10">transation</th>
+                                <th scope="col" class="col-2">time</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr
+                                v-for="transation in formatedTransations"
+                                :key="transation.blockHash"
+                            >
+                                <th scope="row">
+                                    <a
+                                        :href="`https://sepolia.etherscan.io/tx/${transation.blockHash}`"
+                                        target="_blank"
+                                        >{{ transation.blockHash }}</a
+                                    >
+                                </th>
+                                <td>
+                                    {{ transation.date }}
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
-        <hr />
-        <div>
-          <h3>Your transations</h3>
-          <table class="table text-start">
-            <thead>
-              <tr>
-                <th scope="col" class="col-10">transation</th>
-                <th scope="col" class="col-2">time</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-              </tr>
-              <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-              </tr>
-              <tr>
-                <th scope="row">3</th>
-                <td colspan="2">Larry the Bird</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
-import Web3 from 'web3'
+import Web3 from "web3";
+import FaucetBuild from "./../../truffle/build/contracts/Faucet.json";
+import moment from "moment";
 
 export default {
-  data() {
-    return {
-      // FIXME: Provider URL is not worked right now
-      providerURL: import.meta.env.PROVIDERURL || 'https://sepolia.infura.io/v3/${key}',
-      connected: false,
-      contractResult: '',
-      web3: null,
-      accounts: []
-    }
-  },
-  mounted() {
-    this.connect()
-  },
-  methods: {
-    async connect() {
-      let provider = window.ethereum
-      let tmpAcc = []
-      if (typeof provider !== 'undefined') {
-        // MetaMask is installed
-        await provider
-          .request({ method: 'eth_requestAccounts' })
-          .then((accounts) => {
-            this.accounts = accounts
-          })
-          .catch((err) => {
-            console.log(err)
-          })
-      }
-
-      // FIXME: current only support metamask provider
-      this.web3 = new Web3(provider)
-      for (let accAddr of this.accounts) {
-        let balance = await this.getBalance(accAddr)
-        console.log(accAddr, balance)
-      }
-
-      // FIXME: this is the self defined provider
-      // this.web3.setProvider(
-      //   new this.web3.providers.HttpProvider(this.providerURL)
-      // );
+    data() {
+        return {
+            // FIXME: Provider URL is not worked right now
+            contractResult: "",
+            web3: null,
+            accounts: [],
+            FaucetContract: null,
+            message: "",
+            transations: [],
+            formatedTransations: [],
+            recipient: ""
+        };
     },
-    async getBalance(address) {
-      return this.web3.eth.getBalance(address)
+
+    mounted() {
+        this.connect();
+        window.setInterval(this.formatTransations, 1000);
     },
-    async withdraw() {}
-  }
-}
+
+    unmounted() {
+        window.clearInterval(this.formatTransations);
+    },
+
+    methods: {
+        async connect() {
+            let provider = window.ethereum;
+
+            if (typeof provider !== "undefined") {
+                // MetaMask is installed
+
+                provider
+                    .request({ method: "eth_requestAccounts" })
+                    .then((accounts) => {
+                        this.accounts = accounts;
+                        console.log(`current account is ${this.accounts[0]}`);
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    });
+
+                window.ethereum.on("accountsChanged", function (accounts) {
+                    this.accounts = accounts;
+                    console.log(
+                        `current account changed to ${this.accounts[0]}`
+                    );
+                });
+            }
+
+            // FIXME: current only support metamask provider
+            this.web3 = new Web3(provider);
+            for (let accAddr of this.accounts) {
+                let balance = await this.getBalance(accAddr);
+                console.log(accAddr, balance);
+            }
+
+            // FIXME: this is the self defined provider
+            // this.web3.setProvider(
+            //   new this.web3.providers.HttpProvider(this.providerURL)
+            // );
+            // console.log(FaucetContract.methods.withdraw, Web3.utils.toWei('0.1', "ether"))
+            // FaucetContract.methods.withdraw(100000).send({from: this.accounts[0]})
+            // FaucetContract.methods.withdraw(Web3.utils.toWei('0.1', "ether")).then(res => { console.log(res) }).catch((err) => console.log(err))
+        },
+
+        async getBalance(address) {
+            return this.web3.eth.getBalance(address);
+        },
+
+        formatTransations() {
+            this.formatedTransations = this.transations.map((trans) => ({
+                ...trans,
+                date: moment(trans.date).fromNow(),
+            }));
+        },
+
+        appendTransation(blockHash) {
+            this.transations.push({ blockHash: blockHash, date: new Date() });
+            this.formatTransations();
+        },
+
+        async withdraw(amount) {
+            const networkId = await this.web3.eth.net.getId();
+            const FaucetContract = new this.web3.eth.Contract(
+                FaucetBuild.abi,
+                FaucetBuild.networks[networkId].address
+            );
+            return FaucetContract.methods
+                .withdraw(Web3.utils.toWei(amount, "ether"))
+                .send({ from: this.accounts[0] });
+        },
+
+        async withdarwHandler() {
+            const amount = "0.01";
+            this.withdraw(amount).then((msg) => {
+                console.log(msg);
+                this.appendTransation(msg.transactionHash);
+            });
+        },
+    },
+};
 </script>
 
